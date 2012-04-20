d950c21bbe7d42afe830cdfc37f72d8dc980f4b2
Alexander Ewetumo <trinoxf@gmail.com>
2012-04-20 03:21:53 +0100

    query breaker fix

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,3 +1,11 @@
-var design = document.querySelector();
-var html_editor = document.querySelector();
-var js_editor = document.querySelector();
+
+$(function(){
+	
+	var html_editor = $('#editor_view #html-writer div.text-view iframe.text-view-frame');
+	window.html_editor = html_editor;
+
+});
+
+
+
+
---
+++ builds/monster_editor.js
@@ -0,0 +1,1 @@
+$(function(){var a=$("#editor_view #html-writer div.text-view iframe.text-view-frame");window.html_editor=a})
